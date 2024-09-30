3208d8d23daec6d9c988182ced93c68b69a606ad
kyleochata <kyleochata@gmail.com>
2024-09-30 00:25:11 -0700

    install pkg made. Go get

--- install.go
+++ install.go
@@ -1,9 +1,24 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/kyleochata/md_maker/install"
+)
 
+const (
+	fs_im_list fs_im = iota
+	fs_im_ta
+)
+
+type fs_im int
 type Installation_model struct {
 	Answers
+	List           list.Model
+	TextArea       textarea.Model
+	FocusState     fs_im
+	InstallChoices []string
 }
 
 func (m Installation_model) Init() tea.Cmd { return nil }
@@ -19,4 +34,26 @@ func (m Installation_model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 func (m Installation_model) View() string {
 	return "hello"
+}
+
+// =========Helper==================
+var available_installs []string = []string{"C", "C++", "C#", "Golang", "Rails", "Ruby", "TypeScript"}
+
+func New_Install_model(a Answers) tea.Model {
+	list_items := make_list_items(available_installs)
+	list := list.New(list_items, install.CustomDelegate{}, a.Width, a.Height)
+	ta := textarea.New()
+	return Installation_model{Answers: a, List: list, TextArea: ta, FocusState: fs_im_list, InstallChoices: []string{}}
+}
+
+func make_list_items(xs []string) []list.Item {
+	var items []install.Item
+	for _, item := range xs {
+		items = append(items, install.New_list_item(item))
+	}
+	li := make([]list.Item, len(items))
+	for i, item := range items {
+		li[i] = item
+	}
+	return li
 }
---
+++ install/install_l_item.go
@@ -0,0 +1,55 @@
+package install
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Item struct {
+	Name string
+}
+
+func (i Item) Title() string       { return i.Name }
+func (i Item) Description() string { return "" }
+func (i Item) FilterValue() string { return i.Name }
+func New_list_item(s string) Item {
+	return Item{Name: s}
+}
+
+var (
+	normalTextStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("252"))
+	selectedTextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("205")).Bold(true)
+
+// normalBadgeStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("245"))
+// selectedBadgeStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("13"))
+// badgePickedStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("82")).Background(lipgloss.Color("252"))
+)
+
+type CustomDelegate struct{}
+
+func (d CustomDelegate) Height() int                              { return 1 }
+func (d CustomDelegate) Spacing() int                             { return 0 }
+func (d CustomDelegate) Update(ms tea.Msg, m *list.Model) tea.Cmd { return nil }
+
+func (d CustomDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	i, ok := item.(Item)
+	if !ok {
+		return
+	}
+	isSelected := index == m.Index()
+	var cursor string
+	if isSelected {
+		cursor = "--> "
+	} else {
+		cursor = "    "
+	}
+	name := normalTextStyle.Render(i.Name)
+	if isSelected {
+		name = selectedTextStyle.Render(i.Name)
+	}
+	fmt.Fprintf(w, "%s\t%s", cursor, name)
+}
