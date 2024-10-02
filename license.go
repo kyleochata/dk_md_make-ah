ab91f83d521a41ff134aac844d0e5986ffaadb09
kyleochata <kyleochata@gmail.com>
2024-10-02 16:56:50 -0700

    Prompt for no licesnse logic in. Need to render in a list of pos licenses user can use. Prob licesnse pckg

--- license.go
+++ license.go
@@ -38,9 +38,11 @@ func (m License_model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		}
 	}
-	var cmd tea.Cmd
-	if m.TextArea, cmd = m.TextArea.Update(msg); cmd != nil {
-		return m, cmd
+	if m.edit_pregen {
+		var cmd tea.Cmd
+		if m.TextArea, cmd = m.TextArea.Update(msg); cmd != nil {
+			return m, cmd
+		}
 	}
 	return m, nil
 }
@@ -55,12 +57,17 @@ func (m License_model) View() string {
 		} else {
 			uiEl = append(uiEl, gloss.NewStyle().Align(gloss.Left).Render(m.preGenLicense()))
 		}
+	} else {
+		s_no_license := "Seems like there is no LICENSE file in this directory\nIf you have one, please move it to this directory\nCtrl+R: Refresh | Ctrl+L: Generate a license file"
+		uiEl = append(uiEl, gloss.NewStyle().Width(m.Width).Render(s_no_license))
 	}
 	return gloss.JoinVertical(gloss.Center, uiEl...)
 }
+
 func (m *License_model) handleWindowResize(msg tea.WindowSizeMsg) {
 	m.Height, m.Width = msg.Height, msg.Width
 }
+
 func (m License_model) send_to_usage() (tea.Model, tea.Cmd) {
 	return New_Use_model(m.Answers), func() tea.Msg {
 		return tea.WindowSizeMsg{
@@ -69,6 +76,7 @@ func (m License_model) send_to_usage() (tea.Model, tea.Cmd) {
 		}
 	}
 }
+
 func New_License_model(a Answers) tea.Model {
 	found := licenseFileExists()
 	var l_type string
